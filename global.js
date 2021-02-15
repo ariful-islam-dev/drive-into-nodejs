1cfe61b1562e1c5ea77fe758d91ab9766a7003bb
nodeAriful <ariful.node82@gmail.com>
2021-02-15 08:14:45 +0600

    discustion of global object

--- global.js
+++ global.js
@@ -1,9 +1,16 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-setTimeout(()=>{
-    console.log('I am in SetTimOut');
-}, 1000)
+// setTimeout(()=>{
+//     console.log('I am in SetTimOut');
+// }, 1000)
 
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
+var a = 10;
+
+function test(){
+    console.log('I am Test');
+}
+global.test();
