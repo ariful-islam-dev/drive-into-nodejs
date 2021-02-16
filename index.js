dc29efcc841a3c8158ab7c67c74bc10ad37168f2
nodeAriful <ariful.node82@gmail.com>
2021-02-16 16:24:29 +0600

    install dev dependency

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const chalk = require('chalk');
 
 console.log(chalk.red.bold.inverse('I am edited chalk'));
+
+
+console.log(chalk.white.inverse('Another Text'));
+
+console.log(`This is new`);
+console.log('I have install nodemon');
