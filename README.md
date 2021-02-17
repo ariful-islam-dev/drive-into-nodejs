65b2c2e236ea9ecdf416094b25994fd094fd5637
nodeAriful <ariful.node82@gmail.com>
2021-02-17 08:41:39 +0600

    create basice router for page controll

--- README.md
+++ README.md
@@ -6,9 +6,15 @@
 
 * > npm init -y
 * > npm i express
+* > npm i -D nodemon
 
 ### File Create
 - [x] app
 
 ### How to Set environment PORT
   - > export PORT=7070
+### React Route
+  > app.get('/help', (req, res)=>{'help page'})
+  > app.get('/about', (req, res)=>{'about page'})
+  > app.get('/', (req, res)=>{'Home Page'})
+  > app.get('*', (req, res)=>{ res.send('404 Not found')})
--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.get('/about', (req, res)=>{
+    res.send(`<h1>I am about page</h1>`)
+})
+app.get('/help', (req, res)=>{
+    res.send(`<h1>I am help page</h1>`)
+})
+
+
+app.get('/', (req, res)=>{
+    res.send(` <h1>I am listening</h1>`)
+})
+
+app.get('*', (req, res)=>{
+    res.send(` <h1>404 Not Found</h1>`)
+})
+
 
 const PORT = process.env.PORT || 8080;
 app.listen( PORT, ()=>{
