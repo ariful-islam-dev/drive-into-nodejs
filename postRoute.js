95ab046484133d455dd1746e1675e4ecb803216e
nodeAriful <ariful.node82@gmail.com>
2021-02-19 08:06:23 +0600

    get, update and delete a single post

--- postRoute.js
+++ postRoute.js
@@ -1,19 +1,24 @@
 const router = require('express').Router();
 
+//example.com/posts GET
 router.get('/', (req, res) => {
     res.send('Render All Post')
 });
+router.get('/:postId', (req, res)=>{
+    res.send('I am POST = ' + req.params.postId)
+})
 
+//example.com/posts POST
 router.post('/', (req, res) => {
     res.send('Create New Post')
 });
 
-router.put('/', (req, res)=>{
-    res.send('Update Your Post')
+router.put('/:postId', (req, res)=>{
+    res.send('Update Your Existing Post = '+ req.params.postId)
 });
 
-router.delete('/', (req, res)=>{
-    res.send('delete Your Existion Post')
+router.delete('/:postId', (req, res)=>{
+    res.send('delete Your Existion Post = '+ req.params.postId)
 });
 
 module.exports = router
