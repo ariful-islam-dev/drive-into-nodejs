318fc5245c40925825f897191b5eff54c83b6b73
nodeAriful <ariful.node82@gmail.com>
2021-02-25 23:32:25 +0600

    install morgan as logger and ejs as template engine, setup middleware and set views directory as engine views

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
+
+//Setup View Engine
+
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+// Middleware Arrary
+const middleware = [
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({extended: true}),
+    express.json()
+];
+
+app.use(middleware);
 
 
 app.get('/', (req,res)=>{
