922056f5556ac20668e4f2e1f7e351f77f966100
nodeAriful <ariful.node82@gmail.com>
2021-02-28 22:30:18 +0600

    custom confirmation password message

--- playground/validator.js
+++ playground/validator.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { check, validationResult } = require('express-validator');
 
-router.get('/validator', (req, res, nex) => {
+router.get('/validator', (req, res, next) => {
     res.render('playground/signup', { title: 'Validator Playground' })
 })
 router.post('/validator',
@@ -15,13 +15,33 @@ router.post('/validator',
 
         check('email')
             .isEmail()
-            .withMessage(`Please Provide A Valid Email`)
+            .withMessage(`Please Provide A Valid Email`),
+
+        check('password').custom((value) => {
+            if (value.length < 5) {
+                throw new Error(`Password Must be greter then 5 character`);
+            }
+
+            return true;
+        }),
+        check('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+              throw new Error('Password confirmation does not match password');
+            }
+        
+            // Indicates the success of this synchronous custom validator
+            return true;
+          }),
+
+       
+
+
     ],
     (req, res, next) => {
         let errors = validationResult(req)
 
         const formatter = (error) => error.msg;
-
+        // console.log(req.body);
 
         console.log(errors.isEmpty());
         console.log(errors.array());
