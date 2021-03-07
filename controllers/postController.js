d9cfeb9d8e6157ffe0e94591e33ca5ddabd064da
nodeAriful <ariful.node82@gmail.com>
2021-03-08 01:04:42 +0600

    post create succefully and store the database

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,9 @@
+const { validationResult } = require('express-validator')
+const readingTime = require('reading-time')
+const Post = require('../models/Post')
+const Profile = require('../models/Profile')
 const Flash = require("../utils/Flash")
+const errorFormatter = require('../utils/validationErrorFormatter')
 
 exports.createPostGetControler = (req, res, next) => {
 
@@ -6,11 +11,63 @@ exports.createPostGetControler = (req, res, next) => {
         {
             title: 'Create A New Post',
             error: {},
-            flashMessage: Flash.getMessage(req)
+            flashMessage: Flash.getMessage(req),
+            value: {}
         }
     )
 }
 
-exports.createPostPostControler = (req, res, next) => {
-    next()
+exports.createPostPostControler = async (req, res, next) => {
+    let { title, body, tags } = req.body
+    let errors = validationResult(req).formatWith(errorFormatter)
+
+    if (!errors.isEmpty) {
+        return res.render('pages/post/createPost.ejs',
+            {
+                title: 'Create A New Post',
+                error: errors.mapped(),
+                flashMessage: Flash.getMessage(req),
+                value: {
+                    title,
+                    body,
+                    tags
+                }
+            }
+        )
+    }
+    if (tags) {
+        tags = tags.split(',')
+    }
+
+    let readTime = readingTime(body).text
+
+    let post = new Post({
+        title,
+        body,
+        tags,
+        author: req.user._id,
+        thumbnail: '',
+        readTime,
+        likes: [],
+        dislikes: [],
+        comments: []
+    })
+
+    if (req.file) {
+        post.thumbnail = `uploads/${req.file.filename}`
+    }
+
+    try {
+        let createdPost = await post.save()
+        await Profile.findOneAndUpdate(
+            { user: req.user._id },
+            { $push: { 'posts': createdPost } }
+        )
+        req.flash('success', 'Post Created Successfully')
+        return res.redirect(`/posts/edit/${createdPost._id}`)
+    } catch (e) {
+        next(e)
+    }
+
+
 }
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,7 +1,10 @@
 const { createPostGetControler, createPostPostControler } = require('../controllers/postController');
+const postValidator = require('../validator/post/postValidator')
+const { isAuthenticated } = require('../middleware/authMiddleware')
+const upload = require('../middleware/uploadMiddleware')
 const router = require('express').Router();
 
 router.get('/create', createPostGetControler)
-router.post('/create', createPostPostControler)
+router.post('/create', isAuthenticated, upload.single('post-thumbnail'), postValidator, createPostPostControler)
 
 module.exports = router
